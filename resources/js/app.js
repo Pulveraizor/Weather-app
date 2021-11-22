e7307bcd935315f1d882254f1e7b20219ffc517a
Ben Ju <benas.juskus@gmail.com>
2021-11-22 12:10:46 +0200

    Beta 1.0

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,15 +4,24 @@ window.bootstrap = require('bootstrap');
 let regionSelect = document.querySelector('#region');
 let citySelect = document.querySelector('#city');
 
+function getDoubleDigit(a) {
+    if (a < 10) {
+        return `0${a}`;
+    } else {
+        return a;
+    }
+}
+
 function time(){
     let d = new Date();
     let s = d.getSeconds();
     let m = d.getMinutes();
     let h = d.getHours();
 
-    document.querySelector('#clock').innerText = `${h}:${m}:${s}`;
+    document.querySelector('#clock').innerText = `${getDoubleDigit(h)}:${getDoubleDigit(m)}:${getDoubleDigit(s)}`;
+    document.querySelector('#date').innerText = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
 
-}
+};
 
 setInterval(time,1000);
 
@@ -25,7 +34,8 @@ document.addEventListener('DOMContentLoaded', function() {
                 city_option.value = p.administrativeDivision;
                 city_option.innerText = p.name;
                 city_option.classList.add(p.code);
-                city.appendChild(city_option);
+                citySelect.appendChild(city_option);
+                city_option.style.display = 'none';
             }
         }
         let rObj = new Set();
@@ -53,11 +63,67 @@ regionSelect.addEventListener('input', function() {
         if (i.value != this.value) {
             i.style.display = 'none';
             citySelect.value = null;
-        } else {
+        } else if (this.value == '-- Pasirinkite savivaldybę') {
+            i.style.display = 'block';
+        } else  {
             i.style.display = 'block';
         }
     }
 });
+
+
+function createNewCarouselItem(conditionCode, temperature, day, wind) {
+    let newDiv = document.createElement('div');
+    newDiv.classList.add('carousel-item');
+    let newForecast = document.createElement('i');
+    newForecast.classList.add('fas');
+    newForecast.classList.add(conditionCode);
+    let newTemp = document.createElement('h1');
+    newTemp.innerText = `${temperature}°C`;
+    let dayOfForecast = document.createElement('h4');
+    dayOfForecast.innerText = day;
+    let windImage = document.createElement('div');
+    windImage.classList.add('fas');
+    windImage.classList.add('fa-wind');
+    let windSpeed = document.createElement('h5');
+    windSpeed.innerText = `${wind} m/s`;
+    newDiv.appendChild(newForecast);
+    newDiv.appendChild(newTemp);
+    newDiv.appendChild(dayOfForecast);
+    newDiv.appendChild(windImage);
+    newDiv.appendChild(windSpeed);
+    return newDiv;
+};
+
+function setWeatherPicture(conditionCode) {
+    if (conditionCode == 'clear') {
+        return 'fa-sun';
+    } else if (conditionCode == 'isolated-clouds') {
+        return 'fa-cloud-sun';
+    } else if (conditionCode == 'scattered-clouds') {
+        return 'fa-cloud-sun';
+    } else if (conditionCode == 'overcast') {
+        return 'fa-cloud';
+    } else if (conditionCode == 'light-rain') {
+        return 'fa-cloud-rain';
+    } else if (conditionCode == 'moderate-rain') {
+        return 'fa-cloud-rain';
+    } else if (conditionCode == 'heavy-rain') {
+        return 'fa-cloud-showers-heavy';
+    } else if (conditionCode == 'sleet') {
+        return 'fa-cloud-meatball';
+    } else if (conditionCode == 'light-snow') {
+        return 'fa-snowflake';
+    } else if (conditionCode == 'moderate-snow') {
+        return 'fa-snowflake';
+    } else if (conditionCode == 'heavy-snow') {
+        return 'fa-snowflake';
+    } else if (conditionCode == 'fog') {
+        return 'fa-smog';
+    } else {
+        return 'no data';
+    }
+};
 
 citySelect.addEventListener('input', function() {
     let selection = citySelect.options[citySelect.selectedIndex].className;
@@ -65,9 +131,37 @@ citySelect.addEventListener('input', function() {
     fetch(`./weather/places/${selection}`).
     then(response => response.json()).
     then(data => {
-        let resultBox = document.querySelector('#weather_result');
-        let temp = document.createElement('h1');
-        temp.innerHTML = data.forecastTimestamps[0].airTemperature;
-        resultBox.appendChild(temp);
+        console.log(data.forecastTimestamps);
+
+        let time = new Date;
+        let currentDate = `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`;
+        let currentTime = `${time.getHours()}`;
+        let carousel = document.querySelector('.carousel-inner');
+        carousel.innerHTML = '';
+
+        for (let t of data.forecastTimestamps) {
+
+            let forecastTempDate = Array.from(t.forecastTimeUtc).splice(0, 10).join('');
+            let forecastTempTime = Array.from(t.forecastTimeUtc).slice(11).splice(0,2).join('');
+            let forecastDisplayTime = Array.from(t.forecastTimeUtc).slice(11).splice(0, 5).join('');
+
+
+
+            if (currentDate == forecastTempDate && currentTime == forecastTempTime) {
+
+                carousel.appendChild(createNewCarouselItem(setWeatherPicture(t.conditionCode), t.airTemperature, 'Dabar', t.windGust));
+                let firstCarouselItem = document.querySelector('.carousel-inner > div');
+                firstCarouselItem.classList.add('active');
+            }
+            if (currentDate == forecastTempDate && currentTime < forecastTempTime) {
+                carousel.appendChild(createNewCarouselItem(setWeatherPicture(t.conditionCode), t.airTemperature, `Vėliau šiandien ${forecastDisplayTime}`, t.windGust));
+            }
+            if (forecastTempDate.slice(8) == +currentDate.slice(8) + 1) {
+                carousel.appendChild(createNewCarouselItem(setWeatherPicture(t.conditionCode), t.airTemperature, `Rytoj ${forecastDisplayTime}`, t.windGust));
+            }
+            if (forecastTempDate.slice(8) == +currentDate.slice(8) + 2) {
+                carousel.appendChild(createNewCarouselItem(setWeatherPicture(t.conditionCode), t.airTemperature, `Poryt ${forecastDisplayTime}`, t.windGust));
+            }
+        };
     });
-})
+});
